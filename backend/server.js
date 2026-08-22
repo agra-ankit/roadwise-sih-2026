ceeacae9741bd3cb23589be275bc213db373e78c
Akshat Ahlawat <akshatahlawat1818@gmail.com>
2026-08-22 19:16:06 +0530

    chore: add initial backend setup and config files

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+const app = express();
+connectDB()
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "RoadWise Backend is running!"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`RoadWise backend running on port ${PORT}`);
+});
